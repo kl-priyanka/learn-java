aee54b9beadab90f14961048bc9dbe9e29ec5e6d
kl-priyanka <priyankz432@gmail.com>
2026-01-10 11:21:50 +0530

    Added comments

--- HelloAgain.java
+++ HelloAgain.java
@@ -1,5 +1,8 @@
-public class HelloAgain {
-    public static void main(String [] args) {
+public class HelloAgain { //This is a class named HelloAgain
+    public static void main(String [] args) { //This is the main method
         System.out.println("Hello again, World!");
     }
 }
+
+//When you run this file, a file named HelloAgain.class will be created in the same folder, it is called byte code.
+//The byte code is what the Java Virtual Machine (JVM) interprets and runs.

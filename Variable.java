168065ba23d1e61ba34221997a040a752f3dcbfc
kl-priyanka <priyankz432@gmail.com>
2026-01-15 14:30:27 +0530

    15/01/2025

---
+++ Variable.java
@@ -0,0 +1,8 @@
+public class Variable {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 15;
+        int result = a + b;
+        System.out.println(result);
+    }
+}

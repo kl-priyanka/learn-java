bb11944bb75e2530dcecc93a9753e910493f0c45
kl-priyanka <priyankz432@gmail.com>
2026-01-17 16:13:42 +0530

    17/01/2026

---
+++ Variable2.java
@@ -0,0 +1,23 @@
+public class Variable2 {
+    public static void main(String[] args){
+        int a = 22;
+        byte by = 127;
+        // byte by2 = 129; //It produces error because byte ranges from -128 to 127
+        short sh = 5;
+        long l = 95555555l;
+
+        float f = 3.144444444f;
+        double d = 5.888;
+
+        char c = 'p'; //use single quotes only
+
+        boolean b = true;
+
+        char c1 = 'a';
+        char c2 = 55;
+        char c3 = '5'; //takes single digit characters only
+        System.out.println(c1);
+        System.out.println(c2);
+        
+    }
+}
